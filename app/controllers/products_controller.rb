f54371160a9d78527d739aae28a84f4d69c4e358
Diego Cardenas <diego.cardenasaleg@gmail.com>
2024-07-12 09:48:46 -0500

    added CRUD actions to handle products in app

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,49 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(article_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if (@product.update(article_params))
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    puts "llege aca"
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+   def article_params
+    params.require(:product).permit(:title, :description)
+   end
+end
